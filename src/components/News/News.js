5b59114f733ed1125123fa09b643e2577c864c8a
satyam-nickelfox <satyam@nickelfox.com>
2022-01-31 18:59:00 +0530

    style(satyam-nickelfox):update news card style

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,17 +1,16 @@
 import React, { useEffect } from 'react';
 import './News.css'
 import axios from "axios";
-import NewsItem  from './Newsitem';
+import NewsItem from './Newsitem';
 
 function News() {
-    // const [news, setNews] = React.useState([])
     const [articles, setArticles] = React.useState([])
 
     useEffect(() => {
-        
+
         const getArticles = async () => {
-            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in`,{
-                headers:{"Authorization" : "17d9ab7490e5435ea9a1807760eac906"}
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in`, {
+                headers: { "Authorization": "17d9ab7490e5435ea9a1807760eac906" }
             })
             setArticles(response.data.articles)
             console.log(response)
@@ -22,22 +21,47 @@ function News() {
 
 
     return (
-        <div className='cards'>
-           {articles.map((article,i) => {
-                return(
-                    // <div></div>
-                    <NewsItem 
-                        title={article.title}
-                        description={article.description}
-                        url={article.url}
-                        urlToImage={article.urlToImage} 
-                        source = {article.source.name}
-                        key = {i}
-                    />
-                )
-            })}
+        <>
+            {articles.length > 0 ?
 
-        </div>
+                <div className='container'>
+
+                    {articles.map((article, i) => {
+                        return (
+                            <NewsItem
+                                title={article.title}
+                                description={article.description}
+                                url={article.url}
+                                urlToImage={article.urlToImage}
+                                source={article.source.name}
+                                key={i}
+                            />
+                        )
+                    })}
+
+                </div>
+                :
+                <div className="loader"></div>
+            }
+
+
+        </>
+        // <div className='cards'>
+        //    {articles.map((article,i) => {
+        //         return(
+        //             // <div></div>
+        //             <NewsItem 
+        //                 title={article.title}
+        //                 description={article.description}
+        //                 url={article.url}
+        //                 urlToImage={article.urlToImage} 
+        //                 source = {article.source.name}
+        //                 key = {i}
+        //             />
+        //         )
+        //     })}
+
+        // </div>
     );
 }
 
--- src/components/News/Newsitem.js
+++ src/components/News/Newsitem.js
@@ -1,22 +1,44 @@
 import React from 'react'
-
+import {
+    Link
+} from "react-router-dom";
 
 const NewsItem = ({ title, description, url, urlToImage, source }) => {
     return (
-
-        <div className="card-container">
-            <div className='image-container'>
-                <img src={urlToImage} alt={urlToImage} />
+        <div className="card">
+            <div className="card-header">
+                <img src={urlToImage} alt="rover" />
             </div>
-            <div className="card-content">
-                <div className="card-title">
-                    <h3><a href={url}>{title}</a></h3>
+            <div className="card-body">
+                <span className="tag tag-teal">{source}</span>
+                <h4>
+                    <a href={url} target="_blank">{title}</a>
+                </h4>
+                <p>
+                    {description}
+                </p>
+                <div class="user">
+                    <div class="user-info">
+                        <small>2h ago</small>
+                    </div>
                 </div>
-                <p>{description}</p>
-                <h4>-{source}</h4>
+
             </div>
 
         </div>
+        // <div className="card-container">
+        //     <div className='image-container'>
+        //         <img src={urlToImage} alt={urlToImage} />
+        //     </div>
+        //     <div className="card-content">
+        //         <div className="card-title">
+        //             <h3><a href={url}>{title}</a></h3>
+        //         </div>
+        //         <p>{description}</p>
+        //         <h4>-{source}</h4>
+        //     </div>
+
+        // </div>
     )
 }
 
